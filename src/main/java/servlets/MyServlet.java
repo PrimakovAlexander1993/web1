637938190ea0abc15aa554c5bd360762e66ccea1
Alexander Primakov <primakovalexander07041993@gmail.com>
2020-06-16 18:24:17 +0300

    Initial commit

---
+++ src/main/java/servlets/MyServlet.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class MyServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+
+        String val = request.getParameter("value");
+        int valInt = Integer.parseInt(val);
+        writer.println(valInt + valInt);
+        response.setStatus(200);
+
+        if (Double.isNaN(valInt)) {
+            writer.println(0);
+            response.setStatus(400);
+        }
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+    }
+}
